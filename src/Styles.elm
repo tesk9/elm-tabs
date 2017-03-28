9ca79261f4c39e1c81657151ddab3428b06c9809
Tessa Kelly <tessakkelly@gmail.com>
2017-03-27 21:21:13 -0700

    Add some more styling

--- src/Styles.elm
+++ src/Styles.elm
@@ -12,7 +12,6 @@ module Styles
 -}
 
 import Css exposing (..)
-import Css.Colors exposing (..)
 import Css.Namespace exposing (namespace)
 import Html.CssHelpers exposing (Namespace, withNamespace)
 
@@ -30,7 +29,9 @@ type Classes
 snippets : List Snippet
 snippets =
     [ Css.class Container
-        [ width (px 768) ]
+        [ width (px 768)
+        , margin2 (px 20) auto
+        ]
     , Css.class TabList
         [ displayFlex
         , justifyContent spaceBetween
@@ -40,15 +41,19 @@ snippets =
         , borderTopRadius (px 8)
         , minWidth (px 150)
         , textAlign center
+        , backgroundColor lightGray
         ]
     , Css.class SelectedTab
-        [ borderBottomColor (hex "fff")
+        [ borderBottomColor white
+        , backgroundColor white
         ]
     , Css.class TabPanel
         [ border3 (px 1) solid black
+        , borderBottomRadius (px 8)
         , position relative
         , top (px -1)
         , zIndex (int -1)
+        , padding (px 30)
         ]
     ]
 
@@ -59,6 +64,29 @@ borderTopRadius setting =
         [ borderTopLeftRadius setting
         , borderTopRightRadius setting
         ]
+
+
+borderBottomRadius : Length compatible units -> Mixin
+borderBottomRadius setting =
+    mixin
+        [ borderBottomLeftRadius setting
+        , borderBottomRightRadius setting
+        ]
+
+
+black : Color
+black =
+    hex "#000"
+
+
+white : Color
+white =
+    hex "#fff"
+
+
+lightGray : Color
+lightGray =
+    hex "#dbdbdb"
 
 
 { class, classList, id } =
